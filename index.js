38de5fc744f6caae2e4b17f7da52d95eba2b9f47
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2025-10-22 12:06:00 +0200

    creamos un .env para definir el intervalo y definimos las caracteristicas que queremos que diga

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const port = 3000;
 const os = require('os');
+require('dotenv').config();
+
 
 const servidor = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,6 +12,13 @@ const servidor = http.createServer((req, res) => {
 
 servidor.listen(port, () => {
   console.log(`Servidor ejecutándose en http://localhost:${port}/`);
-  console.log(`Nombre del host: ${os.hostname()}`);
+  console.log("Nombre del host: " + os.hostname());
   console.log("Versión de Node.js: " + process.version);
 });
+setInterval(() => {
+    console.log("Uso de cpus: " + os.cpus());
+    console.log("Memoria en uso: " + (os.totalmem() - os.freemem()));
+    console.log("Tiempo de actividad del sistema: " + os.uptime());
+    console.log("Tiempo de uso de node"+ process.uptime());
+}, process.env.INTERVALO);
+
